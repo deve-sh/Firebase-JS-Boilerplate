3a1fad4046886ae49214092e6ec4b09b748a76c2
Devesh Kumar <devesh2027@gmail.com>
2021-12-26 17:32:50 +0530

    Added remote config to boilerplate

---
+++ v9/remoteConfig.js
@@ -0,0 +1,7 @@
+import { getRemoteConfig } from "firebase/remote-config";
+
+const remoteConfig = getRemoteConfig();
+
+export default remoteConfig;
+
+// Reference: https://firebase.google.com/docs/remote-config/get-started?platform=web
